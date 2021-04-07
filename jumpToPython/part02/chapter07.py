2fbb24907d2e7c70f4c73cfead0c6ae2192b1022
Seungjae Lee <dltmdwo430@gmail.com>
2021-04-07 16:34:46 +0900

    complete part02

---
+++ jumpToPython/part02/chapter07.py
@@ -0,0 +1,11 @@
+# 불(bool) 자료형
+# 불 자료형은 True / False 두가지 값만 가질 수 있다.
+a = True
+b = False
+print(type(a))
+print(type(b))
+
+# 불 연산
+# bool 내장함수를 사용하면 자료형의 참과 거짓을 식별 가능
+print(bool('python'))
+print(bool(''))
